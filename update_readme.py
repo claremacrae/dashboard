267b0fe171be5f029e8e7ee0211b356d69d9a6ea
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-14 09:22:06 +0100

    F Add new project ci_playground - default branch is trunk

--- update_readme.py
+++ update_readme.py
@@ -126,6 +126,8 @@ def create_readme():
     builds.add_builds('approvals', 'ApprovalTests.cpp', ['master'], False, 'lf3i76ije89oihi5', 'isidore')
     builds.add_builds('approvals', 'ApprovalTests.cpp.StarterProject', ['master'], False)
 
+    builds.add_builds('claremacrae', 'ci_playground', ['trunk'], True, 'cbksrgvypq5vksy2')
+
     my_branches = ['master', 'more_appveyor_builds']
     builds.add_builds('claremacrae', 'ApprovalTests.cpp', my_branches, True, '37smtsp3a694okv8')
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.StarterProject', my_branches, True, 'mu8a5uib1ha7sx41')
