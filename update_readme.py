4ac7cbd8a22e87677e84fe99f39d87dfb17fdeb9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-19 20:52:36 +0100

    Move Builds class out to separate file

--- update_readme.py
+++ update_readme.py
@@ -1,31 +1,5 @@
-from BranchBuild import BranchBuild
+from Builds import Builds
 
-
-class Builds:
-    def __init__(self):
-        self.builds = []
-
-    def add_build(self, build):
-        self.builds.append(build)
-
-    def add_builds(self, user, project, branches, travis_com, appveyor_token = None, custom_appveyor_user = None):
-        for branch in branches:
-            build = BranchBuild(user, project, branch, travis_com, appveyor_token, custom_appveyor_user)
-            self.add_build(build)
-
-    def write_header(self, stream):
-        stream.write('<a id="top"></a>\n')
-        stream.write('# dashboard\n')
-        stream.write("A space to check build-statuses of projects I'm working on\n")
-        stream.write('\n')
-        stream.write('| User | project | network | branch | [Travis](https://travis-ci.com/claremacrae/) | [Appveyor](https://ci.appveyor.com/projects) |\n')
-        stream.write('| ------------- | ------------- - | -| - | - | - |\n')
-
-    def write_readme(self):
-        with open('README.md', 'w') as stream:
-            self.write_header(stream)
-            for build in self.builds:
-                build.write_row(stream)
 
 def create_readme():
     builds = Builds()
