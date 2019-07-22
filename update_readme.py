1c98bd506ee3510ef333a24db202814b2417122c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-07-22 17:55:41 +0100

    r Divide code up in to functions

--- update_readme.py
+++ update_readme.py
@@ -4,18 +4,28 @@ from Builds import Builds
 def create_readme():
     builds = Builds()
 
-    builds.add_builds('approvals', 'ApprovalTests.cpp', ['master'], False, 'lf3i76ije89oihi5', 'isidore')
-    builds.add_builds('approvals', 'ApprovalTests.cpp.StarterProject', ['master'], False)
+    add_approvals_builds(builds)
+    add_ci_playground(builds)
+    add_my_approvals_projects(builds)
 
-    builds.add_builds('claremacrae', 'ci_playground', ['trunk'], True, 'cbksrgvypq5vksy2')
+    builds.write_readme()
 
+
+def add_my_approvals_projects(builds):
     my_branches = ['master']
     builds.add_builds('claremacrae', 'ApprovalTests.cpp', my_branches, True, '37smtsp3a694okv8')
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.StarterProject', my_branches, True, 'mu8a5uib1ha7sx41')
-
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.Nursery', ['master'], True, 'iqtnpa83t13os98v')
 
-    builds.write_readme()
+
+def add_ci_playground(builds):
+    builds.add_builds('claremacrae', 'ci_playground', ['trunk'], True, 'cbksrgvypq5vksy2')
+
+
+def add_approvals_builds(builds):
+    builds.add_builds('approvals', 'ApprovalTests.cpp', ['master'], False, 'lf3i76ije89oihi5', 'isidore')
+    builds.add_builds('approvals', 'ApprovalTests.cpp.StarterProject', ['master'], False)
+
 
 if __name__ == '__main__':
     # See also https://travis-ci.com/dashboard
