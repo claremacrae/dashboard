affb822d19f34ec288193db5b7daf0b5bc6fb6d6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-23 15:34:51 +0100

    r Inline a variable, to allow different branches in different repos

--- update_readme.py
+++ update_readme.py
@@ -13,9 +13,8 @@ def create_readme():
 
 
 def add_my_approvals_projects(builds):
-    my_branches = ['master']
-    builds.add_builds('claremacrae', 'ApprovalTests.cpp', my_branches, True, '37smtsp3a694okv8')
-    builds.add_builds('claremacrae', 'ApprovalTests.cpp.StarterProject', my_branches, True, 'mu8a5uib1ha7sx41')
+    builds.add_builds('claremacrae', 'ApprovalTests.cpp', ['master'], True, '37smtsp3a694okv8')
+    builds.add_builds('claremacrae', 'ApprovalTests.cpp.StarterProject', ['master'], True, 'mu8a5uib1ha7sx41')
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.Nursery', ['master'], True, 'iqtnpa83t13os98v')
 
 
