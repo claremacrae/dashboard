5524721407f21c384c9853f7b0920383d30ac80a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-23 09:53:59 +0100

    Start grouping by project instead of user

--- update_readme.py
+++ update_readme.py
@@ -4,6 +4,8 @@ from Builds import Builds
 def create_readme():
     builds = Builds()
 
+    add_approval_tests_cpp(builds)
+
     add_approvals_builds(builds)
     add_my_miscellaneous(builds)
     add_my_approvals_projects(builds)
@@ -11,9 +13,11 @@ def create_readme():
 
     builds.write_readme()
 
+def add_approval_tests_cpp(builds):
+    builds.add_builds('approvals', 'ApprovalTests.cpp', ['master'], False, 'lf3i76ije89oihi5', 'isidore')
+    builds.add_builds('claremacrae', 'ApprovalTests.cpp', ['master', 'setup_sanitizers'], True, '37smtsp3a694okv8')
 
 def add_my_approvals_projects(builds):
-    builds.add_builds('claremacrae', 'ApprovalTests.cpp', ['master', 'setup_sanitizers'], True, '37smtsp3a694okv8')
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.StarterProject', ['master'], True, 'mu8a5uib1ha7sx41')
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.Nursery', ['master'], True, 'iqtnpa83t13os98v')
 
@@ -24,7 +28,6 @@ def add_my_miscellaneous(builds):
 
 
 def add_approvals_builds(builds):
-    builds.add_builds('approvals', 'ApprovalTests.cpp', ['master'], False, 'lf3i76ije89oihi5', 'isidore')
     builds.add_builds('approvals', 'ApprovalTests.cpp.StarterProject', ['master'], False)
 
 
