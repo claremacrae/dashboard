8d90bbe94abc6bd6f3db61fca8d8a725cc2094a0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-23 10:11:45 +0100

    Rename function

--- update_readme.py
+++ update_readme.py
@@ -8,7 +8,7 @@ def create_readme():
     add_approval_tests_cpp_starter_project(builds)
 
     add_my_miscellaneous(builds)
-    add_my_approvals_projects(builds)
+    add_approval_tests_cpp_nursery(builds)
     add_catch_projects(builds)
 
     builds.write_readme()
@@ -21,7 +21,7 @@ def add_approval_tests_cpp_starter_project(builds):
     builds.add_builds('approvals', 'ApprovalTests.cpp.StarterProject', ['master'], False)
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.StarterProject', ['master'], True, 'mu8a5uib1ha7sx41')
 
-def add_my_approvals_projects(builds):
+def add_approval_tests_cpp_nursery(builds):
     builds.add_builds('claremacrae', 'ApprovalTests.cpp.Nursery', ['master'], True, 'iqtnpa83t13os98v')
 
 
