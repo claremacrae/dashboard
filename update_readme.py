32e1ee960c41437ac23752d65451191321bb2f82
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-01-28 20:17:33 +0000

    r Inline functions for simplicity

--- update_readme.py
+++ update_readme.py
@@ -5,50 +5,25 @@ from builds import Builds
 def create_readme():
     builds = Builds()
 
-    add_approval_tests_cpp(builds)
-    add_approval_tests_cpp_starter_project(builds)
-    add_approval_tests_cpp_qt(builds)
-    add_approval_tests_cpp_qt_starter_project(builds)
-    add_approval_tests_cpp_nursery(builds)
-    add_approval_tests_cpp_builds(builds)
-
-    # add_catch_projects(builds)
-    add_my_miscellaneous(builds)
-
-    table = BuildTable()
-    table.write_readme(builds)
-
-
-def add_approval_tests_cpp(builds):
     repo = 'ApprovalTests.cpp'
     builds.add_builds('approvals', repo, ['master'], False, 'lf3i76ije89oihi5', 'isidore')
     builds.add_builds('claremacrae', repo, ['master'], True, '37smtsp3a694okv8')  # 'setup_sanitizers'
 
+    repo1 = 'ApprovalTests.cpp.StarterProject'
+    builds.add_builds('approvals', repo1, ['master'], False, 'qx0546k6ii57919w', 'isidore')
+    builds.add_builds('claremacrae', repo1, ['master'], True, 'ytjgybf5r9fviifm')
 
-def add_approval_tests_cpp_starter_project(builds):
-    repo = 'ApprovalTests.cpp.StarterProject'
-    builds.add_builds('approvals', repo, ['master'], False, 'qx0546k6ii57919w', 'isidore')
-    builds.add_builds('claremacrae', repo, ['master'], True, 'ytjgybf5r9fviifm')
+    repo2 = 'ApprovalTests.cpp.Qt'
+    builds.add_builds('approvals', repo2, ['master'], True, 'pf8et0nk1mdajskf', 'isidore')
+    builds.add_builds('claremacrae', repo2, ['master'], True, 'g60qbttap7m5nul2')
 
+    repo3 = 'ApprovalTests.cpp.Qt.StarterProject'
+    builds.add_builds('approvals', repo3, ['master'], False, 'tpitsul9axlv93uk', 'isidore')
+    builds.add_builds('claremacrae', repo3, ['master'], True, 'xe2iwuto0sc342a7')
 
-def add_approval_tests_cpp_qt(builds):
-    repo = 'ApprovalTests.cpp.Qt'
-    builds.add_builds('approvals', repo, ['master'], True, 'pf8et0nk1mdajskf', 'isidore')
-    builds.add_builds('claremacrae', repo, ['master'], True, 'g60qbttap7m5nul2')
+    repo4 = 'ApprovalTests.cpp.Nursery'
+    builds.add_builds('claremacrae', repo4, ['master'], True, 'iqtnpa83t13os98v')
 
-
-def add_approval_tests_cpp_qt_starter_project(builds):
-    repo = 'ApprovalTests.cpp.Qt.StarterProject'
-    builds.add_builds('approvals', repo, ['master'], False, 'tpitsul9axlv93uk', 'isidore')
-    builds.add_builds('claremacrae', repo, ['master'], True, 'xe2iwuto0sc342a7')
-
-
-def add_approval_tests_cpp_nursery(builds):
-    repo = 'ApprovalTests.cpp.Nursery'
-    builds.add_builds('claremacrae', repo, ['master'], True, 'iqtnpa83t13os98v')
-
-
-def add_approval_tests_cpp_builds(builds):
     repos = [
         'ApprovalTests.cpp.Builds',
         'ApprovalTests.cpp.CMakeSamples',
@@ -56,20 +31,21 @@ def add_approval_tests_cpp_builds(builds):
         # 'ApprovalTests.cpp.Scripts',
         'SuperBuildApprovalTests',
     ]
-    for repo in repos:
-        builds.add_builds('claremacrae', repo, ['master'], True)
+    for repo5 in repos:
+        builds.add_builds('claremacrae', repo5, ['master'], True)
 
+    # builds.add_builds('catchorg', 'Catch2', ['master'], False)
+    # builds.add_builds('claremacrae', 'Catch2', ['master'], False, '8m77qos96rmcn6jg')
 
-def add_my_miscellaneous(builds):
     builds.add_builds('claremacrae', 'approval-tests-setup', ['master'], True)
     builds.add_builds('claremacrae', 'ci_playground', ['trunk'], True, 'cbksrgvypq5vksy2')
     builds.add_builds('claremacrae', 'cpp_snippets', ['master'], True, 'hqf8xh615dyp3u4l')
 
+    table = BuildTable()
+    table.write_readme(builds)
 
-# def add_catch_projects(builds):
-#     builds.add_builds('catchorg', 'Catch2', ['master'], False)
-#     builds.add_builds('claremacrae', 'Catch2', ['master'], False, '8m77qos96rmcn6jg')
-#     pass
+
+
 
 
 if __name__ == '__main__':
