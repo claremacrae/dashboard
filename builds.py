f6adf2b07111b8a707ccf33b61acde8a8a1ed9a3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-04 19:24:39 +0000

    add_builds() now takes a AppveyorBuildInfo

--- builds.py
+++ builds.py
@@ -10,8 +10,8 @@ class Builds:
     def add_build(self, build):
         self.builds[build.repo_info.user].append(build)
 
-    def add_builds(self, user, project, branches, travis_com, appveyor_token=None, custom_appveyor_user=None):
+    def add_builds(self, user, project, branches, travis_com, appveyor_build_info):
         for branch in branches:
             build = BranchBuild(RepoInfo(user, project, branch), TravisBuildInfo(travis_com),
-                                AppveyorBuildInfo(appveyor_token, custom_appveyor_user))
+                                appveyor_build_info)
             self.add_build(build)
--- my_builds.py
+++ my_builds.py
@@ -1,3 +1,6 @@
+from branch_build import AppveyorBuildInfo
+
+
 def add_all_builds(builds):
     add_official_approval_test_repos(builds)
 
@@ -13,25 +16,26 @@ def add_all_builds(builds):
 
 def add_official_approval_test_repos(builds):
     repo = 'ApprovalTests.cpp'
-    builds.add_builds('approvals', repo, ['master'], False, 'lf3i76ije89oihi5', 'isidore')
-    builds.add_builds('claremacrae', repo, ['master'], True, '37smtsp3a694okv8')  # 'setup_sanitizers'
+    builds.add_builds('approvals', repo, ['master'], False, AppveyorBuildInfo('lf3i76ije89oihi5', 'isidore'))
+    builds.add_builds('claremacrae', repo, ['master'], True,
+                      AppveyorBuildInfo('37smtsp3a694okv8'))  # 'setup_sanitizers'
 
     repo1 = 'ApprovalTests.cpp.StarterProject'
-    builds.add_builds('approvals', repo1, ['master'], False, 'qx0546k6ii57919w', 'isidore')
-    builds.add_builds('claremacrae', repo1, ['master'], True, 'ytjgybf5r9fviifm')
+    builds.add_builds('approvals', repo1, ['master'], False, AppveyorBuildInfo('qx0546k6ii57919w', 'isidore'))
+    builds.add_builds('claremacrae', repo1, ['master'], True, AppveyorBuildInfo('ytjgybf5r9fviifm'))
 
     repo2 = 'ApprovalTests.cpp.Qt'
-    builds.add_builds('approvals', repo2, ['master'], True, 'pf8et0nk1mdajskf', 'isidore')
-    builds.add_builds('claremacrae', repo2, ['master'], True, 'g60qbttap7m5nul2')
+    builds.add_builds('approvals', repo2, ['master'], True, AppveyorBuildInfo('pf8et0nk1mdajskf', 'isidore'))
+    builds.add_builds('claremacrae', repo2, ['master'], True, AppveyorBuildInfo('g60qbttap7m5nul2'))
 
     repo3 = 'ApprovalTests.cpp.Qt.StarterProject'
-    builds.add_builds('approvals', repo3, ['master'], False, 'tpitsul9axlv93uk', 'isidore')
-    builds.add_builds('claremacrae', repo3, ['master'], True, 'xe2iwuto0sc342a7')
+    builds.add_builds('approvals', repo3, ['master'], False, AppveyorBuildInfo('tpitsul9axlv93uk', 'isidore'))
+    builds.add_builds('claremacrae', repo3, ['master'], True, AppveyorBuildInfo('xe2iwuto0sc342a7'))
 
 
 def add_my_experimental_approvals_repos(builds):
     repo4 = 'ApprovalTests.cpp.Nursery'
-    builds.add_builds('claremacrae', repo4, ['master'], True, 'iqtnpa83t13os98v')
+    builds.add_builds('claremacrae', repo4, ['master'], True, AppveyorBuildInfo('iqtnpa83t13os98v'))
     repos = [
         'ApprovalTests.cpp.Builds',
         'ApprovalTests.cpp.CMakeSamples',
@@ -40,16 +44,16 @@ def add_my_experimental_approvals_repos(builds):
         'SuperBuildApprovalTests',
     ]
     for repo5 in repos:
-        builds.add_builds('claremacrae', repo5, ['master'], True)
+        builds.add_builds('claremacrae', repo5, ['master'], True, AppveyorBuildInfo())
 
 
 def add_my_random_repos(builds):
-    builds.add_builds('claremacrae', 'approval-tests-setup', ['master'], True)
-    builds.add_builds('claremacrae', 'ci_playground', ['trunk'], True, 'cbksrgvypq5vksy2')
-    builds.add_builds('claremacrae', 'cpp_snippets', ['master'], True, 'hqf8xh615dyp3u4l')
+    builds.add_builds('claremacrae', 'approval-tests-setup', ['master'], True, AppveyorBuildInfo())
+    builds.add_builds('claremacrae', 'ci_playground', ['trunk'], True, AppveyorBuildInfo('cbksrgvypq5vksy2'))
+    builds.add_builds('claremacrae', 'cpp_snippets', ['master'], True, AppveyorBuildInfo('hqf8xh615dyp3u4l'))
 
 
 def add_boost_ut_repos(builds):
     repo = 'ut'
-    builds.add_builds('boost-experimental', repo, ['master'], False)
-    builds.add_builds('claremacrae', repo, ['master', 'clare_learning'], True, 'ab4jv9x8kveev0n4')
+    builds.add_builds('boost-experimental', repo, ['master'], False, AppveyorBuildInfo())
+    builds.add_builds('claremacrae', repo, ['master', 'clare_learning'], True, AppveyorBuildInfo('ab4jv9x8kveev0n4'))
