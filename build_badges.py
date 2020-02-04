699f1aa3da92240a4b7a8f00d20817781bc6b5b2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-04 20:37:46 +0000

    Introduce variable

--- build_badges.py
+++ build_badges.py
@@ -2,14 +2,15 @@ class BuildBadges:
 
     @staticmethod
     def write_row(stream, branch_build):
-        branch = branch_build.repo_info.branches[0]
+        repo_info = branch_build.repo_info
+        branch = repo_info.branches[0]
         links = [
-            branch_build.travis_build_info.status(branch_build.repo_info, branch),
-            branch_build.appveyor_build_info.status(branch_build.repo_info, branch),
-            branch_build.github_build_info.status(branch_build.repo_info, branch),
+            branch_build.travis_build_info.status(repo_info, branch),
+            branch_build.appveyor_build_info.status(repo_info, branch),
+            branch_build.github_build_info.status(repo_info, branch),
         ]
         stream.write(F'\n')
-        stream.write(F'{branch_build.repo_info.user}/{branch_build.repo_info.project}\n')
+        stream.write(F'{repo_info.user}/{repo_info.project}\n')
         stream.write(F'\n')
         for link in links:
             stream.write(F'{link} \n')
