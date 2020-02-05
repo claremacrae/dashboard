055d47aba5bf9121d6cff74a218ebdcf59871428
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-05 10:16:18 +0000

    F Mark my repos as forks, where appropriate

--- my_builds.py
+++ my_builds.py
@@ -18,23 +18,23 @@ def add_official_approval_test_repos(builds):
     repo = 'ApprovalTests.cpp'
     builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False),
                            AppveyorConfig('lf3i76ije89oihi5', 'isidore'))
-    builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True),
+    builds.add_forked_repo('claremacrae', repo, ['master'], TravisConfig(True),
                            AppveyorConfig('37smtsp3a694okv8'))  # 'setup_sanitizers'
 
     repo = 'ApprovalTests.cpp.StarterProject'
     builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False),
                            AppveyorConfig('qx0546k6ii57919w', 'isidore'))
-    builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('ytjgybf5r9fviifm'))
+    builds.add_forked_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('ytjgybf5r9fviifm'))
 
     repo = 'ApprovalTests.cpp.Qt'
     builds.add_source_repo('approvals', repo, ['master'], TravisConfig(True),
                            AppveyorConfig('pf8et0nk1mdajskf', 'isidore'))
-    builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('g60qbttap7m5nul2'))
+    builds.add_forked_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('g60qbttap7m5nul2'))
 
     repo = 'ApprovalTests.cpp.Qt.StarterProject'
     builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False),
                            AppveyorConfig('tpitsul9axlv93uk', 'isidore'))
-    builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('xe2iwuto0sc342a7'))
+    builds.add_forked_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('xe2iwuto0sc342a7'))
 
 
 def add_my_experimental_approvals_repos(builds):
@@ -68,5 +68,5 @@ def add_my_random_repos(builds):
 def add_boost_ut_repos(builds):
     repo = 'ut'
     builds.add_source_repo('boost-experimental', repo, ['master'], TravisConfig(False))
-    builds.add_source_repo('claremacrae', repo, ['master', 'clare_learning'], TravisConfig(True),
+    builds.add_forked_repo('claremacrae', repo, ['master', 'clare_learning'], TravisConfig(True),
                            AppveyorConfig('ab4jv9x8kveev0n4'))
