75a24e1e80873d49697a5be724a8aab77ea43057
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-05 10:13:43 +0000

    r Reformat code

--- my_builds.py
+++ my_builds.py
@@ -16,20 +16,24 @@ def add_all_builds(builds):
 
 def add_official_approval_test_repos(builds):
     repo = 'ApprovalTests.cpp'
-    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False), AppveyorConfig('lf3i76ije89oihi5', 'isidore'))
+    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False),
+                           AppveyorConfig('lf3i76ije89oihi5', 'isidore'))
     builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True),
                            AppveyorConfig('37smtsp3a694okv8'))  # 'setup_sanitizers'
 
     repo = 'ApprovalTests.cpp.StarterProject'
-    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False), AppveyorConfig('qx0546k6ii57919w', 'isidore'))
+    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False),
+                           AppveyorConfig('qx0546k6ii57919w', 'isidore'))
     builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('ytjgybf5r9fviifm'))
 
     repo = 'ApprovalTests.cpp.Qt'
-    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(True), AppveyorConfig('pf8et0nk1mdajskf', 'isidore'))
+    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(True),
+                           AppveyorConfig('pf8et0nk1mdajskf', 'isidore'))
     builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('g60qbttap7m5nul2'))
 
     repo = 'ApprovalTests.cpp.Qt.StarterProject'
-    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False), AppveyorConfig('tpitsul9axlv93uk', 'isidore'))
+    builds.add_source_repo('approvals', repo, ['master'], TravisConfig(False),
+                           AppveyorConfig('tpitsul9axlv93uk', 'isidore'))
     builds.add_source_repo('claremacrae', repo, ['master'], TravisConfig(True), AppveyorConfig('xe2iwuto0sc342a7'))
 
 
@@ -55,8 +59,10 @@ def add_my_experimental_approvals_repos(builds):
 def add_my_random_repos(builds):
     # clone of other people's work - using CMake's FetchContent:
     # builds.add_builds('claremacrae', 'approval-tests-setup', ['master'])
-    builds.add_source_repo('claremacrae', 'ci_playground', ['trunk'], TravisConfig(True), AppveyorConfig('cbksrgvypq5vksy2'))
-    builds.add_source_repo('claremacrae', 'cpp_snippets', ['master'], TravisConfig(True), AppveyorConfig('hqf8xh615dyp3u4l'))
+    builds.add_source_repo('claremacrae', 'ci_playground', ['trunk'], TravisConfig(True),
+                           AppveyorConfig('cbksrgvypq5vksy2'))
+    builds.add_source_repo('claremacrae', 'cpp_snippets', ['master'], TravisConfig(True),
+                           AppveyorConfig('hqf8xh615dyp3u4l'))
 
 
 def add_boost_ut_repos(builds):
