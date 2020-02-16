f2553184fc581760f9d63aaf49f37108856f7c92
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-16 20:34:37 +0000

    Extract method to reduce repetition

--- scripts/github_repo_info.py
+++ scripts/github_repo_info.py
@@ -14,22 +14,25 @@ class GitHubRepoInfo:
         self.branches = branches
         self.type = type
 
+    def user_account_url(self) -> str:
+        return f"https://github.com/{self.user}"
+
     def user_repos_link(self) -> str:
         text = self.user
-        url = f"https://github.com/{self.user}?tab=repositories"
+        url = f"{self.user_account_url()}?tab=repositories"
         return dashboard_utilities.hyperlinked_text(text, url)
 
     def project_link(self) -> str:
         text = self.project
-        url = f"https://github.com/{self.user}/{self.project}/"
+        url = f"{self.user_account_url()}/{self.project}/"
         return dashboard_utilities.hyperlinked_text(text, url)
 
     def network_link(self) -> str:
         text = 'network'
-        url = f"https://github.com/{self.user}/{self.project}/network"
+        url = f"{self.user_account_url()}/{self.project}/network"
         return dashboard_utilities.hyperlinked_text(text, url)
 
     def branch_link(self, branch: str) -> str:
         text = branch
-        url = f"https://github.com/{self.user}/{self.project}/commits/{branch}"
+        url = f"{self.user_account_url()}/{self.project}/commits/{branch}"
         return dashboard_utilities.hyperlinked_text(text, url)
