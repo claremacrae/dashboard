629203271f42a319db9535cad5fdfec101297266
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-16 20:22:15 +0000

    Rename method to make it public (to add type hints)

--- scripts/all_repos.py
+++ scripts/all_repos.py
@@ -20,15 +20,15 @@ class AllRepos:
                         travis_build_info: TravisBuildConfig = TravisBuildConfig(True),
                         appveyor_build_info: AppveyorBuildConfig = AppveyorBuildConfig(),
                         github_build_info: GitHubBuildConfig = GitHubBuildConfig()) -> None:
-        self.__add_repo(appveyor_build_info, branches, project, travis_build_info, github_build_info, 'Source', user)
+        self.add_repo(appveyor_build_info, branches, project, travis_build_info, github_build_info, 'Source', user)
 
     def add_forked_repo(self, user: str, project: str, branches: List[str],
                         travis_build_info: TravisBuildConfig = TravisBuildConfig(True),
                         appveyor_build_info: AppveyorBuildConfig = AppveyorBuildConfig(),
                         github_build_info: GitHubBuildConfig = GitHubBuildConfig()) -> None:
-        self.__add_repo(appveyor_build_info, branches, project, travis_build_info, github_build_info, 'Fork', user)
+        self.add_repo(appveyor_build_info, branches, project, travis_build_info, github_build_info, 'Fork', user)
 
-    def __add_repo(self, appveyor_build_info, branches, project, travis_build_info, github_build_info, repo_type, user):
+    def add_repo(self, appveyor_build_info, branches, project, travis_build_info, github_build_info, repo_type, user):
         repo = RepoAndBuilds(GitHubRepoInfo(user, project, branches, repo_type), travis_build_info,
                              appveyor_build_info, github_build_info)
         self.__store_repo(repo)
