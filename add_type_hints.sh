f0f6162c6430cf6783653c70ebc32ebc9cc482ee
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-16 19:13:08 +0000

    Add a script to run monkeytype to detect types

---
+++ add_type_hints.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# https://instagram-engineering.com/let-your-code-type-hint-itself-introducing-open-source-monkeytype-a855c7284881
+monkeytype run update_markdown_files.py
+monkeytype list-modules
+
+#monkeytype apply scripts.all_repos
+#monkeytype apply scripts.ci_configs
+#monkeytype apply scripts.create_build_badges
+#monkeytype apply scripts.create_build_table
+#monkeytype apply scripts.dashboard_utilities
+#monkeytype apply scripts.my_builds
+#monkeytype apply scripts.repo_and_builds
