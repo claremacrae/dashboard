254dd09e37b472ac0bc135dbcc37cbc62eff373e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-16 20:20:21 +0000

    Remove hard-coded list of modules from script

--- add_type_hints.sh
+++ add_type_hints.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 # https://instagram-engineering.com/let-your-code-type-hint-itself-introducing-open-source-monkeytype-a855c7284881
-#monkeytype run update_markdown_files.py
-#monkeytype list-modules
-
-monkeytype apply scripts.all_repos
-monkeytype apply scripts.ci_configs
-monkeytype apply scripts.create_build_badges
-monkeytype apply scripts.create_build_table
-monkeytype apply scripts.dashboard_utilities
-monkeytype apply scripts.my_builds
-monkeytype apply scripts.repo_and_builds
+rm monkeytype.sqlite3
+monkeytype run update_markdown_files.py
+for module in $(monkeytype list-modules) ; do
+  monkeytype apply $module
+done
