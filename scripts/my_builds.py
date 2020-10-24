1fab1b76127cf13745ccc8211385281fb08cb377
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-24 11:50:17 +0100

    Remove CppUTest_support branch

--- scripts/my_builds.py
+++ scripts/my_builds.py
@@ -15,7 +15,7 @@ def add_all_repos(builds: AllRepos) -> None:
 
 def add_official_approval_test_repos(builds: AllRepos) -> None:
     repo = 'ApprovalTests.cpp'
-    builds.add_source_repo('approvals', repo, ['master', 'CppUTest_support'],
+    builds.add_source_repo('approvals', repo, ['master'],
                            TravisBuildConfig(False),
                            AppveyorBuildConfig('lf3i76ije89oihi5', 'isidore'))
     builds.add_forked_repo('claremacrae', repo, ['master', 'github_action_python_builds'], TravisBuildConfig(True),
