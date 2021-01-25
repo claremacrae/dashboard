1669d65620cf484e9cf4ae799f3ab07e9c7275eb
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-01-25 23:55:25 +0000

    Fix type warnings when passing in None

--- scripts/all_repos.py
+++ scripts/all_repos.py
@@ -18,15 +18,15 @@ class AllRepos:
 
     def add_source_repo(self, user: str, project: str, branches: List[str],
                         appveyor_build_info: Union[AppveyorBuildConfig, None] = AppveyorBuildConfig(),
-                        github_build_info: GitHubBuildConfig = GitHubBuildConfig()) -> None:
+                        github_build_info: Union[GitHubBuildConfig, None] = GitHubBuildConfig()) -> None:
         self.add_repo(appveyor_build_info, branches, project, github_build_info, 'Source', user)
 
     def add_forked_repo(self, user: str, project: str, branches: List[str],
-                        appveyor_build_info: AppveyorBuildConfig = AppveyorBuildConfig(),
-                        github_build_info: GitHubBuildConfig = GitHubBuildConfig()) -> None:
+                        appveyor_build_info: Union[AppveyorBuildConfig, None] = AppveyorBuildConfig(),
+                        github_build_info: Union[GitHubBuildConfig, None] = GitHubBuildConfig()) -> None:
         self.add_repo(appveyor_build_info, branches, project, github_build_info, 'Fork', user)
 
-    def add_repo(self, appveyor_build_info: AppveyorBuildConfig, branches: List[str], project: str,
-                 github_build_info: GitHubBuildConfig, repo_type: str, user: str) -> None:
+    def add_repo(self, appveyor_build_info: Union[AppveyorBuildConfig, None], branches: List[str], project: str,
+                 github_build_info: Union[GitHubBuildConfig, None], repo_type: str, user: str) -> None:
         repo = RepoAndBuilds(GitHubRepoInfo(user, project, branches, repo_type), appveyor_build_info, github_build_info)
         self.__store_repo(repo)
