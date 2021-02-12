86f75c1346d34dde3fce05430b4db01814444015
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-02-12 11:09:04 +0000

    Add more branches

--- scripts/my_builds.py
+++ scripts/my_builds.py
@@ -49,7 +49,7 @@ def add_official_approval_test_repos_python(builds: AllRepos) -> None:
 
 
 def add_my_experimental_approvals_repos(builds: AllRepos) -> None:
-    builds.add_source_repo('claremacrae', 'commandline-videostore-cpp', ['webinar_prep', 'DR1'])
+    builds.add_source_repo('claremacrae', 'commandline-videostore-cpp', ['webinar_prep', 'W1-endpoint', 'W2', 'DR1'])
 
     misc = 'Miscellaneous'
     builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.CMakeSamples', ['main'], language=misc)
