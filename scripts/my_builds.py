9613329661926187c1c56b3a3f5f557eebd2dcb5
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-22 21:47:07 +0000

    Start dividing similar methods up in to classes - add TestFrameworkRepos

--- scripts/my_builds.py
+++ scripts/my_builds.py
@@ -9,9 +9,7 @@ def add_all_repos(builds: AllRepos) -> None:
     add_my_random_repos(builds)
 
     # Other things I've forked - alphabetical by account name
-    # add_boost_ut_repos(builds)
-    # add_catch_repos(builds)
-    # add_doctest_repos(builds)
+    TestFrameworkRepos.add_all_repos(builds)
 
 
 class CppApprovalTests:
@@ -81,16 +79,26 @@ def add_my_random_repos(builds: AllRepos) -> None:
                            None, language=language)
 
 
-def add_catch_repos(builds):
-    repo = builds.add_source_repo('catchorg', 'Catch2', ['master'])
-    builds.add_forked_repo(repo, AppveyorBuildConfig('8m77qos96rmcn6jg'))
+class TestFrameworkRepos:
+    @staticmethod
+    def add_all_repos(builds: AllRepos):
+        # TestFrameworkRepos.add_boost_ut_repos(builds)
+        # TestFrameworkRepos.add_catch_repos(builds)
+        # TestFrameworkRepos.add_doctest_repos(builds)
+        pass
 
+    @staticmethod
+    def add_catch_repos(builds: AllRepos):
+        repo = builds.add_source_repo('catchorg', 'Catch2', ['master'])
+        builds.add_forked_repo(repo, AppveyorBuildConfig('8m77qos96rmcn6jg'))
 
-def add_boost_ut_repos(builds):
-    repo = builds.add_source_repo('boost-experimental', 'ut', ['master'])
-    builds.add_forked_repo(repo, AppveyorBuildConfig('ab4jv9x8kveev0n4'), ['master', 'clare_learning'])
+    @staticmethod
+    def add_boost_ut_repos(builds: AllRepos):
+        repo = builds.add_source_repo('boost-experimental', 'ut', ['master'])
+        builds.add_forked_repo(repo, AppveyorBuildConfig('ab4jv9x8kveev0n4'), ['master', 'clare_learning'])
 
-
-def add_doctest_repos(builds: AllRepos) -> None:
-    repo = builds.add_source_repo('onqtam', 'doctest', ['master', 'dev'], github_build_info=GitHubBuildConfig(['CI']))
-    builds.add_forked_repo(repo, AppveyorBuildConfig('y3ylbpuv79souy6e'), ['master', 'dev'])
+    @staticmethod
+    def add_doctest_repos(builds: AllRepos) -> None:
+        repo = builds.add_source_repo('onqtam', 'doctest', ['master', 'dev'],
+                                      github_build_info=GitHubBuildConfig(['CI']))
+        builds.add_forked_repo(repo, AppveyorBuildConfig('y3ylbpuv79souy6e'), ['master', 'dev'])
