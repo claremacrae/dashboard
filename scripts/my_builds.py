70f5866463c367fd46b3873d79979546d5564e22
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-23 08:16:15 +0000

    Move methods to ClareRepos

--- scripts/my_builds.py
+++ scripts/my_builds.py
@@ -58,36 +58,37 @@ class PythonApprovalTests:
 class ClareRepos:
     @staticmethod
     def add_all_my_repos(builds: AllRepos):
-        add_my_experimental_approvals_repos(builds)
-        add_my_random_repos(builds)
+        ClareRepos.add_my_experimental_approvals_repos(builds)
+        ClareRepos.add_my_random_repos(builds)
 
+    @staticmethod
+    def add_my_experimental_approvals_repos(builds: AllRepos) -> None:
+        builds.add_source_repo('claremacrae', 'commandline-videostore-cpp',
+                               ['starting-point', 'complete-run', 'webinar'],
+                               github_build_info=GitHubBuildConfig(['build', 'on-push-do-doco']),
+                               language="Webinar")
 
-def add_my_experimental_approvals_repos(builds: AllRepos) -> None:
-    builds.add_source_repo('claremacrae', 'commandline-videostore-cpp', ['starting-point', 'complete-run', 'webinar'],
-                           github_build_info=GitHubBuildConfig(['build', 'on-push-do-doco']),
-                           language="Webinar")
+        misc = 'Miscellaneous'
+        builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.CMakeSamples', ['main'], language=misc)
+        builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.Demos', ['main'], language=misc)
+        builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.Nursery', ['main'], language=misc)
 
-    misc = 'Miscellaneous'
-    builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.CMakeSamples', ['main'], language=misc)
-    builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.Demos', ['main'], language=misc)
-    builds.add_source_repo('claremacrae', 'ApprovalTests.cpp.Nursery', ['main'], language=misc)
+        repos = [
+            # 'ApprovalTests.cpp.Scripts',
+            'SuperBuildApprovalTests',
+        ]
+        for repo in repos:
+            builds.add_source_repo('claremacrae', repo, ['main'], None, None, language=misc)
 
-    repos = [
-        # 'ApprovalTests.cpp.Scripts',
-        'SuperBuildApprovalTests',
-    ]
-    for repo in repos:
-        builds.add_source_repo('claremacrae', repo, ['main'], None, None, language=misc)
-
-
-def add_my_random_repos(builds: AllRepos) -> None:
-    language = 'Miscellaneous'
-    # clone of other people's work - using CMake's FetchContent:
-    # builds.add_builds('claremacrae', 'approval-tests-setup', ['master'])
-    builds.add_source_repo('claremacrae', 'ci_playground', ['trunk'], AppveyorBuildConfig('cbksrgvypq5vksy2'),
-                           language=language)
-    builds.add_source_repo('claremacrae', 'cpp_snippets', ['main'], AppveyorBuildConfig('hqf8xh615dyp3u4l'),
-                           None, language=language)
+    @staticmethod
+    def add_my_random_repos(builds: AllRepos) -> None:
+        language = 'Miscellaneous'
+        # clone of other people's work - using CMake's FetchContent:
+        # builds.add_builds('claremacrae', 'approval-tests-setup', ['master'])
+        builds.add_source_repo('claremacrae', 'ci_playground', ['trunk'], AppveyorBuildConfig('cbksrgvypq5vksy2'),
+                               language=language)
+        builds.add_source_repo('claremacrae', 'cpp_snippets', ['main'], AppveyorBuildConfig('hqf8xh615dyp3u4l'),
+                               None, language=language)
 
 
 class TestFrameworkRepos:
