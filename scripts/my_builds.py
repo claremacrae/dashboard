563267466618b8e799e60a477cd7f58d0d479640
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-23 08:26:47 +0000

    Move class out from my_builds.py in to separate file

--- scripts/my_builds.py
+++ scripts/my_builds.py
@@ -1,4 +1,5 @@
 from scripts.CppApprovalTests import CppApprovalTests
+from scripts.PythonApprovalTests import PythonApprovalTests
 from scripts.implementation.all_repos import AllRepos
 from scripts.implementation.ci_configs import AppveyorBuildConfig, GitHubBuildConfig
 
@@ -10,28 +11,6 @@ def add_all_repos(builds: AllRepos) -> None:
 
     # Other things I've forked - alphabetical by account name
     TestFrameworkRepos.add_all_repos(builds)
-
-
-class PythonApprovalTests:
-    @staticmethod
-    def add_all_repos(builds: AllRepos) -> None:
-        # PythonApprovalTests.add_approvals_repos(builds)
-        pass
-
-    @staticmethod
-    def add_approvals_repos(builds):
-        python = 'Python'
-        default_workflows = ['Test', 'on-push-do-doco']  # We only show the publishing workflow in source repo
-        parent_git_hub_build_config = GitHubBuildConfig(default_workflows + ['Upload Python Package'], False)
-        repo = builds.add_source_repo('approvals', 'ApprovalTests.Python', ['master'], None,
-                                      parent_git_hub_build_config,
-                                      language=python)
-        fork_git_hub_build_config = GitHubBuildConfig(default_workflows, False)
-        builds.add_forked_repo(repo, github_build_info=fork_git_hub_build_config)
-
-        repo = builds.add_source_repo('approvals', 'ApprovalTests.Python.PytestPlugin', ['master'], None,
-                                      GitHubBuildConfig(['Test'], False), language=python)
-        builds.add_forked_repo(repo)
 
 
 class ClareRepos:
